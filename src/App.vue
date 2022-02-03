945c22dfcc672ed4131bec2dc9222e7be0980c3b
vmarcella <cenz@cenz.io>
2022-02-02 20:40:10 -0800

    [add] keyframe for coloring the entire background.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,33 @@
 </template>
 
 <style>
+
+@keyframes bgcolor {
+  0% {
+    background-color: #45a3e5;
+  }
+
+  30% {
+    background-color: #66bf39;
+  }
+
+  60%  {
+  background-color: #eb670f;
+  }
+
+  90% {
+    background-color: #f35;
+  }
+
+  100% {
+    background-color: #864cbf;
+  }
+}
+
+body {
+  -webkit-animation: bgcolor 20s infinite;
+}
+
 #app {
   font-family: 'Roboto Mono', monospace;
   font-size: 13px;
