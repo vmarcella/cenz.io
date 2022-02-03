1b4e20400722ba86a841b2fd25f509ed49427b57
vmarcella <cenz@cenz.io>
2022-02-02 21:11:33 -0800

    [update] navbar properties with classes & tags.

--- src/App.vue
+++ src/App.vue
@@ -40,22 +40,11 @@ body {
 #app {
   font-family: 'Roboto Mono', monospace;
   font-size: 13px;
+  width: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <ul>
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
-      <li><router-link to="/contact">Contact</router-link></li>
+  <div id="navbar">
+    <ul class="navbar-items">
+      <li class="navbar-item"><router-link to="/">Home</router-link></li>
+      <li class="navbar-item"><router-link to="/about">About</router-link></li>
+      <li class="navbar-item"><router-link to="/contact">Contact</router-link></li>
     </ul>
   </div>
 </template>
@@ -15,4 +15,34 @@ export default class NavBar extends Vue {}
 </script>
 
 <style local>
+
+#navbar {
+  width: 100%;
+}
+
+/* Navbar item styling */
+.navbar-items {
+  list-style-type: none;
+  background-color: #333;
+  border-radius: 8px;
+}
+
+.navbar-item {
+  font-size: 2em;
+}
+
+/* Navbar item links */
+.navbar-item a {
+
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.navbar-item a:hover {
+  background-color: #111;
+}
+
 </style>
