5778e677de659a70a3869c3d043b79669c213f63
vmarcella <cenz@cenz.io>
2022-02-02 21:58:56 -0800

    [add] container and experiment with setting up properties on the vue-class-component.

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="width: {{width}}%; height: {{height}}%">
+  </div>
+
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+import { Component, Prop } from 'vue-property-decorator';
+
+export default class Container extends Vue {
+  @Prop({ default: 100 })
+  width!: number;
+
+  @Prop({ default: 100 })
+  height!: number;
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
-    <Navbar />
+  <Container />
+  <Navbar />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import Navbar from '@/components/Navbar.vue';
+import Container from '@/components/Container.vue';
 
 @Options({
   components: {
     Navbar,
+    Container,
   },
 })
 export default class Home extends Vue {}
