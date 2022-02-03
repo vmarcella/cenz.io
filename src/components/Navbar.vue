1ad34d4e03695ca929b5862e70fec1845c7deef9
vmarcella <cenz@cenz.io>
2022-02-02 17:56:46 -0800

    [update] home to remove the use of the HelloWorld component & update App.vue to only render the router-view.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <ul>
-      <li><a>Home</a></li>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Vue } from 'vue-class-component';
 
 export default class NavBar extends Vue {}
 </script>
