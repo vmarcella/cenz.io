6cae86662ce5ef9f9afaadc448d7eb000f0580e0
vmarcella <cenz@cenz.io>
2022-02-11 16:14:19 -0800

    [update] post preview styling.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
+  <div id="post-previews">
     <PostPreview
+      class="preview"
       v-for="post in posts"
       :key="post.name"
       :title=post.name
       :description=post.description
       :date=post.date />
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,3 +23,18 @@ export default class Home extends Vue {
   posts: Array<PostData> = GetAllPosts();
 }
 </script>
+
+<style scoped>
+#post-previews {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.preview {
+  width: 50%;
+  margin: auto;
+  border: 20px;
+}
+</style>
