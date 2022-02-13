71e811930890c4859679d973cab7a8826cd8e582
vmarcella <cenz@cenz.io>
2022-02-13 15:26:39 -0800

    [add] about me section.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <h2 id="about-header">About me</h2>
+    <p id="about-description">
+      Hello, my name is Vincenzo!
+      I'm currently a Software Engineer at Microsoft with a passion for
+      System Design, Software Architecture, and Optimization. I love talking
+      about anything STEM related, and especially
+      and would like to talk to me about, please head over to the
+      contact section to get in touch. :)
+      Most of my work can be found on Github.
+    </p>
+  </div>
+  <div id="skills-achievements">
+    <h2 id="skills-header">Skills & Achievements</h2>
+
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+export default class About extends Vue { }
+</script>
+
+<style scoped>
+
+#about {
+  width: 75%;
+  margin: auto;
+}
+
+#about-header {
+  margin-top: 5px;
+  font-size: 2.5em;
+  text-align: left;
+}
+
+#about-description {
+  font-size: 1.5em;
+  text-align: left;
+}
+
+#skills-achievements {
+  text-align: center;
+}
+
+#skills-header {
+  font-size: 2.5em;
+}
+
+</style>
