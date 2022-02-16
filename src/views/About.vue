52672e9ee8edd929ef49644014b3294d933c5c4d
vmarcella <cenz@cenz.io>
2022-02-15 17:15:21 -0800

    [update] description.

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,10 +2,11 @@
   <div id="about">
     <h2 id="about-header">About me</h2>
     <p id="about-description">
-      Hello, I'm Vincenzo!
-      I'm currently a Software Engineer at Microsoft with a passion for
-      System Design, Software Architecture, and Optimization. I love talking
-      about anything STEM related, and especially
+      Hello, I'm Vincenzo! I'm a Software Engineer for the Linux Provisioning
+      team at Microsoft. My primary passions are System Design, Software
+      Architecture, and Optimization, but I love to learn new things about
+      mathematics and physics whenever I get the opportunity to.
+      Outside of work I like to lift weights, going on walks,
       and would like to talk to me about, please head over to the
       contact section to get in touch. :)
       Most of my work can be found on Github.
